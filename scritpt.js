59e78d4ac428b042e75a8ef9ede18ae8d655b05a
Charleston Campos <charlinhocjc@gmail.com>
2023-01-26 20:25:30 -0300

    site finalizado

--- scritpt.js
+++ scritpt.js
@@ -1,20 +1,4 @@
-function onSubmit() {
-    var form = new FormData(document.getElementById("form"));
-
-
-    console.log(form.get("name"))
-    console.log(form.get("email"))
-    console.log(form.get("celular"))
-    console.log(form.get("caixasPersonalizadas"))
-    console.log(form.get("etiquetasEscolares"))
-    console.log(form.get("topodeBolo"))
-    console.log(form.get("tubetes"))
-    console.log(form.get("msg"))
-
-    
-    
-
-
-
-    return;
+function onSelectProduct(e) {
+    const product = e.getElementsByTagName('p')[0].innerText;
+    alert('O produto selecionado foi: ' + product)
 }
